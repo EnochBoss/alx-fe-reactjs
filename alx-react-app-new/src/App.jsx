4b1bdd154a4c74277a40c354a2da6855539e9e17
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-20 22:02:05 +0000

    add coumter with react hook useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Enoch" age="28" bio="Fulfiled through problem-solving" />
+      <Counter />
 
     </>
   )
