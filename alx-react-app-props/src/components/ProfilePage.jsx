2354e3bc04d9c6a02e85503370a91872c5009725
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-20 23:57:12 +0000

    add useContext in place of props

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import UserInfo from './UserInfo';
+import UserContext from './UserContext';
 
 function ProfilePage() {
   return <UserInfo />;
