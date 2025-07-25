da91eb4a0219d9911faecdbfe96b335f1360313c
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-25 17:01:26 +0000

    add my-company multipage react project with react routes implementation

---
+++ my-company/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav>
+        <ul style={{listStyle:'none'}}>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Services' element={<Services />} />
+          <Route path='/Contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
