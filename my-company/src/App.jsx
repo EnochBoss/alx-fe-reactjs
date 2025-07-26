6ccd6ac6c3d032a40dfa0eef51858cd78f6dce2e
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-26 00:18:25 +0000

    add my-company multipage react project with react routes implementation

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -13,15 +14,7 @@ function App() {
 
   return (
     <>
-      <nav>
-        <ul style={{listStyle:'none'}}>
-          <li><a href="/home">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
