b2dd1e881e62c93423c0a7344251f044ecd39587
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-26 15:48:49 +0000

    add my-company multipage react project with react routes implementation

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div>
         <nav>
-        <ul style={{listStyle:'none'}}>
+        <ul style={{listStyle:'none', backgroundColor:'black', display:'flex', justifyContent:'space-between'}}>
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
