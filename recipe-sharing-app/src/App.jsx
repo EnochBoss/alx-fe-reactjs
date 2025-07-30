d2c637176c080439c3ae8568089e0dc1b9a3271b
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-30 01:11:53 +0000

    add recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+import useRecipeStore from './components/recipeStore'
+
+function App() {
+  return (
+    <>
+      <div>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+    </>
+  )
+}
+
+export default App
