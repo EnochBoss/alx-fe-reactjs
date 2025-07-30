e602ca72794a17fecc66451ad86abd3d03c60a2d
EnochBoss <enoch.bosomtwe@gmail.com>
2025-07-30 02:14:06 +0000

    add more components and functionality

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,11 +5,26 @@ import './App.css'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import useRecipeStore from './components/recipeStore'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+
+
+const RecipeDetailsPage = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
+};
 
 function App() {
   return (
     <>
       <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<RecipeDetails />} />
+            <Route path="/recipe/:id" element={<RecipeDetailsPage />} />
+          </Routes>
+        </BrowserRouter>
+
         <AddRecipeForm />
         <RecipeList />
       </div>
