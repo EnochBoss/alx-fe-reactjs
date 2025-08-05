802c0eeee6582ae9fb0f99009a2a3c03d2eb48f0
EnochBoss <enoch.bosomtwe@gmail.com>
2025-08-05 23:37:50 +0000

    add User Profile component with initial styling using tailwind classes

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className="bg-red-400 hover:bg-green-400 hover:text-white hover:text-md hover:cursor-pointer">
-        Click on the Vite and React logos to learn more
-      </p>
+      <UserProfile />
     </>
   )
 }
